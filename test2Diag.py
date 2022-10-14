3efdda1e103a3055a960d76f3f5057752b0ad36f
BMT <matthieu.basto@socomec.com>
2022-10-14 10:40:39 +0200

    definition of functions

--- test2Diag.py
+++ test2Diag.py
@@ -7,22 +7,41 @@ diagfile = open('autoDaig2.mmd','w')
 
 
 #init mermaid class diagram
-diagfile.write('classDiagram \n')
+def initMermaid():
+    diagfile.write('classDiagram \n')
 
 
 #recupere les noms des modules/directories
-zNameDir = os.listdir(originPath)
-for i in range(len(zNameDir)):
-    diagfile.write('Program --> ' + zNameDir[i] + '\n')
+def initRepo():
+    zNameDir = os.listdir(originPath)
+    for i in range(len(zNameDir)):
+        diagfile.write('Program --> ' + zNameDir[i] + '\n')
 
 #recupere les path de tout les fichiers du repertoire original
-for root, dirs, files in os.walk(originPath):        
-    for i in files:
-        nameFile = os.path.join(root, i)
-        diagfile.write(nameFile + '\n')
-        zCompletListFiles.append(nameFile)
+def pathList(racine,i):
+            nameFile = os.path.join(racine, i)
+            # diagfile.write(nameFile + '\n')
+            zCompletListFiles.append(nameFile)
 
 
+
+def main():
+    initMermaid()
+    for root, dirs, files in os.walk(originPath):        
+        for i in files:
+            pathList(root,i)
+    test = zCompletListFiles[1].split('\\')
+    initRepo()
+
+
+
+
+
+main()
+
+print(zCompletListFiles[1])
+test = zCompletListFiles[1].split('\\')
+print (test[1])
         
 
 
